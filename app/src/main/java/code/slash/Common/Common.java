55fc3c30d2a3ccc6a5a0c8119361b82dfff81222
Sohil Luhar <sohil.l@somaiya.edu>
2020-01-30 19:45:12 +0530

    Initial Commit

---
+++ app/src/main/java/code/slash/Common/Common.java
@@ -0,0 +1,8 @@
+package code.slash.Common;
+
+
+import code.slash.User;
+
+public class Common {
+    public static User currentuser;
+}
---
+++ app/src/main/java/code/slash/Home.java
@@ -0,0 +1,40 @@
+package code.slash;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+    TextView wlcmUser;
+    Button btnSearch,btnSell;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+//        wlcmUser=(TextView)findViewById(R.id.wlcmUser);
+//        btnSearch=(Button)findViewById(R.id.searchBook);
+//        btnSell=(Button)findViewById(R.id.sellBook);
+//
+//        String msg="Hello ! "+Common.currentuser.getName();
+//        wlcmUser.setText(msg);
+//        btnSell.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(Home.this, "You pressed browse button", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        btnSearch.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(Home.this, "You pressed create button", Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+    }
+
+
+}
